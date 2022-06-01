c9c9fb0ad97357ebdcfb210fdd54776e7c8fa53a
Elena <helen57_97@mail.ru>
2022-06-01 11:04:34 +0300

    create models

---
+++ u8191676-lab2/database/migrations/2022_06_01_065051_create_buyers_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('buyers', function (Blueprint $table) {
+            $table->id();
+            $table->string('firstName');
+            $table->string('lastName');
+            $table->string('email')();
+            $table->string('phone')();
+            $table->boolean('isBlocked')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('buyers');
+    }
+};
